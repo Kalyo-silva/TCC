07831171e09e9279b5c5660242a66133d6bbc327
Kalyo-Airan-da-Silva <kalyosilva@gmail.com>
2026-08-07 09:01:38 -0300

    Execução das Avaliaões

--- resources/views/pages/avaliacao/⚡execute.blade.php
+++ resources/views/pages/avaliacao/⚡execute.blade.php
@@ -9,8 +9,52 @@ new class extends Component
     public $dimensao = 0;
     public $indicador = 0;
 
+    public $lastDimensao = false;
+    public $firstDimensao = true;
+    
+    public $lastIndicador = false;
+    public $firstIndicador = false;
+
     public function mount($id){
         $this->avaliacao = avaliacao::findOrFail($id);
+    }
+
+    public function nextDimensao(){        
+        if ($this->dimensao == $this->avaliacao->instrumento->dimensoes->count() -1) {
+            $this->lastDimensao = true;
+        }else{  
+            $this->dimensao += 1;
+            $this->indicador = 0;
+            $this->firstDimensao = false;
+        }
+        
+    }
+    public function previousDimensao(){     
+        if ($this->dimensao == 0) {
+            $this->firstDimensao = true;
+        }else{  
+            $this->dimensao -= 1;
+            $this->indicador = 0;
+            $this->lastDimensao = false;
+        }
+    }
+
+    public function nextIndicador(){        
+        if ($this->indicador == $this->avaliacao->instrumento->dimensoes[$this->dimensao]->indicadores->count() -1) {
+            $this->lastIndicador = true;
+        }else{  
+            $this->indicador += 1;
+            $this->firstIndicador = false;
+        }
+        
+    }
+    public function previousIndicador(){     
+        if ($this->indicador == 0) {
+            $this->firstIndicador = true;
+        }else{  
+            $this->indicador -= 1;
+            $this->lastIndicador = false;
+        }
     }
 };
 ?>
@@ -48,8 +92,11 @@ new class extends Component
         <flux:heading>Indicador</flux:heading>
         <flux:card class="p-0 w-full flex flex-col overflow-hidden">
             <div class="flex gap-2 items-center p-4 w-full">
-                <flux:icon.chart-bar/>
-                {{'# ' . $this->avaliacao->instrumento->dimensoes[$this->dimensao]->indicadores[$this->indicador]->sequencia . ' - ' . $this->avaliacao->instrumento->dimensoes[$this->dimensao]->indicadores[$this->indicador]->descricao }}
+                <flux:icon.chart-bar class="size-8"/>
+                <div>
+                    <flux:heading>Indicador  # {{ $this->avaliacao->instrumento->dimensoes[$this->dimensao]->indicadores[$this->indicador]->sequencia }} </flux:heading>
+                    <flux:text>{{ $this->avaliacao->instrumento->dimensoes[$this->dimensao]->indicadores[$this->indicador]->descricao }}</flux:text>
+                </div>
             </div>
             @foreach ($this->avaliacao->instrumento->dimensoes[$this->dimensao]->indicadores[$this->indicador]->criterios as $crit)     
                 <div class="flex gap-2 items-start p-4 w-full border-t border-zinc-600 pl-12 text-justify">
@@ -69,9 +116,10 @@ new class extends Component
         <flux:card>
         </flux:card>
     </div>
-
     <div class="flex gap-2 flex-row-reverse">
-        <flux:button icon:trailing="arrow-right" tr>Proxímo</flux:button>
-        <flux:button icon="arrow-left">Anterior</flux:button>
+        <flux:button :disabled="$this->lastDimensao"   icon:trailing="chevron-double-right" wire:click='nextDimensao()' >Proxíma Dimensão</flux:button>
+        <flux:button :disabled="$this->lastIndicador"  icon:trailing="arrow-right"          wire:click='nextIndicador()'>Proxímo</flux:button>
+        <flux:button :disabled="$this->firstIndicador" icon="arrow-left"                    wire:click='previousIndicador()'>Anterior</flux:button>
+        <flux:button :disabled="$this->firstDimensao"  icon="chevron-double-left"           wire:click='previousDimensao()'>Dimensão Anterior</flux:button>
     </div>
 </div>
